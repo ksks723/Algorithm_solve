0fcac620557080267c52e7b944ca50c2a9f2fce7
ksks723 <ksks723@gmail.com>
2021-11-05 14:24:53 +0900

    gitignore

--- algo/src/Main.java
+++ algo/src/Main.java
@@ -1,5 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Wolrd");
+    public static void main(String[] args) throws IOException {
+        BufferedReader re = new BufferedReader(new InputStreamReader(System.in));
+        int min = Integer.parseInt(re.readLine());
+        int max = Integer.parseInt(re.readLine());
+        for(int i =min; i< max+1; i++){
+            if (isPrime(i)){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static Boolean isPrime(int num){
+        if( num < 2) {
+            return false;
+        }
+        for(int i=2; i*i<=num; i++){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
