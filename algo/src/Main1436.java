e94ee9f2c062cf7c02a0381eab3604ecdf1c4c28
ksks723 <ksks723@gmail.com>
2021-11-07 03:03:27 +0900

    1436

---
+++ algo/src/Main1436.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main1436 {
+        public static void main(String[] args) throws IOException {
+            BufferedReader re = new BufferedReader(new InputStreamReader(System.in));
+            int N = Integer.parseInt(re.readLine());
+            int title = 665;
+            while(N > 0){
+                if(Integer.toString(++title).contains("666")){
+                    --N;
+                }
+            }
+            System.out.println(title);
+        }
+}
