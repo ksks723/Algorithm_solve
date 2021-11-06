9a3405dbcfadc04adea4b84339c1e6313ba1cb58
ksks723 <ksks723@gmail.com>
2021-11-06 19:44:03 +0900

    algo

---
+++ algo/src/test/Main10818.java
@@ -0,0 +1,23 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+public class Main10818 {
+        public static void main(String[] args) throws IOException {
+            BufferedReader re = new BufferedReader(new InputStreamReader(System.in));
+            int number = Integer.parseInt(re.readLine());
+            String[] array = re.readLine().split(" ");
+
+            int[] ar = new int[number];
+            for(int i = 0; i< number ; i++){
+                ar[i] = Integer.parseInt(array[i]);
+            }
+
+            Arrays.sort(ar);
+
+            System.out.println(ar[0] + " " + ar[number-1]);
+
+        }
+}
--- algo/src/test/Main1929.java
+++ algo/src/test/Main1929.java
@@ -4,12 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Main1929 {
     public static void main(String[] args) throws IOException {
         BufferedReader re = new BufferedReader(new InputStreamReader(System.in));
-        int min = Integer.parseInt(re.readLine());
-        int max = Integer.parseInt(re.readLine());
-        for(int i =min; i< max+1; i++){
+        String userInput = re.readLine();
+        String[] answer = userInput.split(" ");
+        for(int i =Integer.parseInt(answer[0]); i< Integer.parseInt(answer[1])+1; i++){
             if (isPrime(i)){
                 System.out.println(i);
             }
@@ -20,7 +20,7 @@ public class Main {
         if( num < 2) {
             return false;
         }
-        for(int i=2; i*i<=num; i++){
+        for(int i=2;i<46341&& i*i<=num; i++){
             if(num % i == 0){
                 return false;
             }
