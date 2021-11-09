087ab7d6221c729b77d7df2646541659cbfb9e00
ksks723 <ksks723@gmail.com>
2021-11-10 00:41:20 +0900

    commit

--- algo/src/BAEKJOON/Main1181.java
+++ algo/src/BAEKJOON/Main1181.java
@@ -9,7 +9,10 @@ public class Main1181 {
         public static void main(String[] args) throws IOException {
             BufferedReader re = new BufferedReader(new InputStreamReader(System.in));
             int number = Integer.parseInt(re.readLine());
-
+            String[] words = new String[number];
+            for(int i = 0; i<number; i++)  {
+                words[i] = re.readLine();
+            }
 
         }
 }
