8d0036249f9c7dc3bc19f7fd896058f1fef609fa
ksks723 <ksks723@gmail.com>
2021-11-09 18:32:15 +0900

    feat:bufferedReader

---
+++ algo/src/BAEKJOON/Main1181.java
@@ -0,0 +1,15 @@
+package BAEKJOON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main1181 {
+        public static void main(String[] args) throws IOException {
+            BufferedReader re = new BufferedReader(new InputStreamReader(System.in));
+            int number = Integer.parseInt(re.readLine());
+
+
+        }
+}
